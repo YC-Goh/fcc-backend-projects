27c58f7c852ea0f7c971990047c2923302259e3e
Goh Yeow Chong <gohyc1993@gmail.com>
2023-02-04 22:01:38 +0800

    fix: first pass at user requirements

--- boilerplate-project-urlshortener-main/index.js
+++ boilerplate-project-urlshortener-main/index.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
+let express = require('express');
+let cors = require('cors');
+let app = express();
+let mongoose = require('mongoose');
+let dns = require('dns');
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGODB_URI);
+
+// The URL template (Schematic, Model, Whatever)
+let urlScheme = mongoose.Schema({
+  root: {type: String, required: true, unique: true}, 
+  id: {type: Number, required: true, unique: true}
+});
+
+let urlModel = mongoose.model('originalUrl', urlScheme);
 
 app.use(cors());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,8 +27,45 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/:shorturl', function(req, res) {
+  let shortUrl = req.params.shorturl;
+  urlModel.findOne({id: shortUrl}).then(function (data) {
+    res.redirect(data.root);
+  }).catch(function (err) {
+    res.json({error: 'No such short URL in the database'});
+  })
+});
+
+app.post('/api/shorturl', function(req, res) {
+  let originalUrl = req.body.url;
+  let invalidUrlErr = {error: 'invalid url'};
+  if (originalUrl) {
+    let urlPattern = /^https?:\/\/([a-z0-9]+(\.[a-z0-9\-]+)+)\/?$/i
+    if (originalUrl.match(urlPattern)) {
+      let originalHostName = originalUrl.replace(urlPattern, '$1');
+      dns.lookup(originalHostName, function (err, add, ipv) {
+        if (err) {
+          res.json(invalidUrlErr);
+        } else {
+          urlModel.findOne({root: originalUrl}).then(function (data) {
+            res.json({original_url: data.root, short_url: data.id});
+          }).catch((err) => {
+            urlModel.countDocuments({}).then(function (count) {
+              urlModel.create({root: originalUrl, id: count}).then(function(data) {
+                res.json({original_url: data.root, short_url: data.id});
+              }).catch(function(err) {
+                res.json(invalidUrlErr)
+              })
+            }).catch(function (err) {
+              res.json(invalidUrlErr);
+            });
+          });
+        };
+      });
+    } else {
+      res.json(invalidUrlErr);
+    };
+  };
 });
 
 app.listen(port, function() {
