cdae56ea849121f80207c92a119cb193d5c68a32
Goh Yeow Chong <gohyc1993@gmail.com>
2023-02-06 13:24:03 +0800

    feat: getting exercise log functional

--- boilerplate-project-exercisetracker-main/index.js
+++ boilerplate-project-exercisetracker-main/index.js
@@ -63,7 +63,7 @@ app.post('/api/users', function (req, res) {
   });
 });
 
-app.post('/api/users', function (req, res) {
+app.get('/api/users', function (req, res) {
   userModel.find({}).select({username: 1, _id: 1}).then(function (data) {
     res.json(data);
   }).catch(function (err) {
@@ -122,9 +122,18 @@ app.post('/api/users/:_id/exercises', function (req, res) {
 
 app.get('/api/users/:_id/logs', function (req, res, next) {
   let {_id} = req.params;
+  if (_id.match(/^[0-9a-z_]+$/)) {
+    next();
+  } else {
+    res.json({error: 'Invalid ID Format'});
+  };
+});
+
+app.get('/api/users/:_id/logs', function (req, res, next) {
+  let {_id} = req.params;
   userModel.findById(_id).then(function (data) {
-    req.body._id = data._id;
-    req.body.username = data.username;
+    req.query._id = data._id;
+    req.query.username = data.username;
     next();
   }).catch(function (err) {
     res.json(err);
@@ -132,26 +141,78 @@ app.get('/api/users/:_id/logs', function (req, res, next) {
 });
 
 app.get('/api/users/:_id/logs', function (req, res, next) {
-  let {from} = req.body;
+  let {from} = req.query;
   if (from) {
     if (from.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/)) {
-      from = (new Date(from)).toISOString().slice(0,10);
-      if (from === 'Invalid Date') {
+      from = new Date(from);
+      if (from.toString() === 'Invalid Date') {
         res.json({error: 'Invalid Date'});
       } else {
-        from = (new Date(from)).toISOString.slice(0,10);
+        req.query.from = from.toISOString().slice(0,10);
+        console.log(req.query);
         next();
       };
     } else {
       res.json({error: 'from field has invalid date format'})
     };
   } else {
+    req.query.from = (new Date(0)).toISOString().slice(0,10);
+    console.log(req.query);
     next();
   };
 });
 
+app.get('/api/users/:_id/logs', function (req, res, next) {
+  let {to} = req.query;
+  if (to) {
+    if (to.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/)) {
+      to = new Date(to);
+      if (to.toString() === 'Invalid Date') {
+        res.json({error: 'Invalid Date'});
+      } else {
+        req.query.to = to.toISOString().slice(0,10);
+        console.log(req.query);
+        next();
+      };
+    } else {
+      res.json({error: 'to field has invalid date format'})
+    };
+  } else {
+    req.query.to = (new Date()).toISOString().slice(0,10);
+    console.log(req.query);
+    next();
+  };
+});
 
+app.get('/api/users/:_id/logs', function (req, res, next) {
+  let {limit} = req.query;
+  if (limit) {
+    if (limit.match(/^[0-9]+$/i) && limit !== '0') {
+      req.query.limit = parseInt(limit);
+      next();
+    } else {
+      res.json({error: 'Invalid limit'});
+    };
+  } else {
+    next();
+  }
+});
 
 app.get('/api/users/:_id/logs', function (req, res) {
-  let {from, to, limit, _id, username} = req.body;
+  console.log(req.query);
+  let {from, to, limit, _id, username} = req.query;
+  let SELECT = {date: 1, duration: 1, description: 1};
+  let FIND = {username};
+  let findHandler = function (err, log) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json({username, count: log.length, _id, log});
+    };
+  };
+  if (limit) {
+    exerciseModel.find(FIND).select(SELECT).where('date').gte(from).lte(to).limit(limit).exec(findHandler);
+  } else {
+    exerciseModel.find(FIND).select(SELECT).where('date').gte(from).lte(to).exec(findHandler);
+  };
 });
